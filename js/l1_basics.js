ecf0df1629fd5c9f929d63a16a9cf856e6cd9c2d
shakir11 <shilgam@mail.ru>
2017-08-20 22:18:08 -0700

    Configured Atom editor with ESLint and the AirBnB style guide rules

--- js/l1_basics.js
+++ js/l1_basics.js
@@ -1,6 +1,9 @@
 // Visualize JavaScript code execution:
 // http://pythontutor.com/javascript.html#mode=edit
 
+// Configuring Atom editor with ESLint and the AirBnB style guide rules:
+// http://www.acuriousanimal.com/2016/08/14/configuring-atom-with-eslint.html
+
 const squareOfSum = (a, b) => {
   return (Math.pow(a, 2) + 2 * a * b + Math.pow(b, 2));
 }
