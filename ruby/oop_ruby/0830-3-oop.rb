e46f788b987a6b20d93220b8877e04948f4100da
shakir11 <shilgam@mail.ru>
2017-08-30 23:09:36 +0300

    object methods

--- ruby/oop_ruby/0830-3-oop.rb
+++ ruby/oop_ruby/0830-3-oop.rb
@@ -5,8 +5,18 @@ class Dog
     @name = name
     @age = age
   end
+
+  def gaf
+    p 'gaf, gaf, gaf!'
+  end
 end
 
 dog_object = Dog.new('Chappi', 2)
 
 p "get name:    #{dog_object.name}"
+
+if (dog_object.respond_to?('gaf'))
+  dog_object.gaf
+else
+  puts "Sorry, don't understand."
+end
